22e4f456b974946225a4fc1c4d6ee2824a54130d
Mitchel Song <songminsu2363@gmail.com>
2023-08-31 16:53:20 +0900

    스프링 MVC(웹 페이지 만들기) - 상품 도메인 개발

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,25 @@
+package hello.itemservice.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+
+    // Integer를 쓴 이유 : 가격이나 수량에는 null값이 들어갈 수 있기 때문
+    private Integer price;
+    private Integer quantity;
+
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
